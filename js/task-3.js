435f6f6bff31287d4d35affd7e9dc8c61ba2f367
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-01-21 10:45:29 +0200

    4

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {}
+  padStart(str) {}
+  padBoth(str) {}
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
